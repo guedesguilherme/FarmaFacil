df48c05255d961848d8edb459b48c1c14cc48411
guilherme-guedes130105 <gryan.guedes@gmail.com>
2024-11-28 20:50:32 -0300

    Att Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import './App.css'
 
